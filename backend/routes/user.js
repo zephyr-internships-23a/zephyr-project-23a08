68eb2ddded51e3a5a9572468f17620b72b1ac6ac
nao <iromnao602gmail.com>
2024-04-05 02:31:52 +0530

    Implemented Update Password and Profile Feature

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register,login, followUser, logout } = require("../controllers/user");
+const { register,login, followUser, logout, updatePassword, updateProfile } = require("../controllers/user");
 const {isAuthenticated} = require("../middlewares/auth")
 
 const router = express.Router();
@@ -11,5 +11,9 @@ router.route("/login").post(login);
 router.route("/logout").get(logout);
 
 router.route("/follow/:id").get(isAuthenticated,followUser)
+
+router.route("/update/password").put(isAuthenticated, updatePassword);
+
+router.route("/update/profile").put(isAuthenticated, updateProfile);
 
 module.exports = router;
